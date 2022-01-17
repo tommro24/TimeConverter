84f23a512b632ee4974ad1d4f174305711c0c435
Tomek Mroziński <mrozoo@icloud.com>
2022-01-17 22:02:13 +0100

    Initial Commit

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Tomek on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
